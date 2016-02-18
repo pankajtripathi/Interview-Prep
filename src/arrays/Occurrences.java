d0d27f6043299f8b36571e7d8bb7e05534229e3b
pankajtripathi.k@gmail.com <pankajtripathi.k@gmail.com>
2016-02-18 00:31:13 -0500

    Added Occurrences.java

---
+++ src/arrays/Occurrences.java
@@ -0,0 +1,39 @@
+package arrays;
+
+import java.util.Scanner;
+
+/**
+ * @author pankajtripathi
+ */
+public class Occurrences {
+    static long occurrence(long n,long i){
+        long count=0;
+        while(n!=0){
+            if(n%10 == i)
+                count++;
+            n=n/10;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the number");
+        long n=scanner.nextLong();
+        long countForNum=-1,maxCount=-1,maxCountNum = 0;
+
+        for(long i=0;i<=9;i++){
+            countForNum = occurrence(n,i);
+            if(countForNum != 0)
+            System.out.println("Occurrence for "+i+" is "+countForNum);
+
+            if(countForNum > maxCount) {
+                maxCount = countForNum;
+                maxCountNum = i;
+            }
+        }
+
+        System.out.println("Number with max occurrences is "+maxCountNum);
+        scanner.close();
+
+    }
+}
