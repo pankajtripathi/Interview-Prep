645b9ae6d985deada82790f2011897f8f642d10e
pankajtripathi.k@gmail.com <pankajtripathi.k@gmail.com>
2016-03-10 18:19:54 -0500

    linked list and arrays updated

---
+++ src/linked_list/MergeSortedList.java
@@ -0,0 +1,59 @@
+package linked_list;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MergeSortedList {
+	public static void main(String[] args) {
+		List<Integer> list1=new LinkedList<>();
+		list1.add(1);
+		list1.add(3);
+		list1.add(5);
+		List<Integer> list2=new LinkedList<>();
+		list2.add(2);
+		list2.add(4);
+		list2.add(6);
+		list2.add(8);
+		list2.add(10);
+		List<Integer> resList=mergeList(list1,list2);
+		System.out.println("Merged List:\n");
+		for(Integer num:resList)
+			System.out.print(num+"  ");
+	}
+
+	private static List<Integer> mergeList(List<Integer> list1,
+			List<Integer> list2) {
+		int indexL=0,indexR=0,indexRes=0;
+		int maxSize=list1.size()+list2.size();
+		List<Integer> resList=new LinkedList<>();
+
+		while(indexRes!=maxSize){
+			//till both list are empty
+			if(indexL<list1.size() && indexR<list2.size()){
+				if(list1.get(indexL)<=list2.get(indexR)){
+					resList.add(indexRes, list1.get(indexL));
+					indexL++;
+					indexRes++;
+				}
+				else{
+					resList.add(indexRes, list2.get(indexR));
+					indexR++;
+					indexRes++;
+				}
+			}
+			//if there are no elements in second list
+			else if(indexL<list1.size()){
+				resList.add(indexRes, list1.get(indexL));
+				indexL++;
+				indexRes++;
+			}
+			//if there are no elements in first list
+			else if(indexR<list2.size()){
+				resList.add(indexRes, list2.get(indexR));
+				indexR++;
+				indexRes++;
+			}
+		}
+		return resList;
+	}
+}
---
+++ src/linked_list/Solution.java
@@ -0,0 +1,19 @@
+package linked_list;
+
+public class Solution {
+	public Node mergeTwoLists(Node l1, Node l2) {
+		if(l1==null) return l2;
+		if(l2==null) return l1;
+		if(l1.data < l2.data){
+			l1.link=mergeTwoLists(l1.link,l2);
+			return l1;
+		}
+		else{
+			l2.link=mergeTwoLists(l2.link,l1);
+			return l2;
+		}
+	}
+}
+
+
+
